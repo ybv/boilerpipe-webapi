220dad0c37285e67c5d3934f269d0b68f764068f
Vaibhav Krishna Irugu Guruswamy <ybv@Vaibhavs-MacBook-Pro.local>
2014-03-01 22:21:31 -0600

    heroku

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
  
 app.get('/ttyl/:url', boiler.respondToURL);
  
-var port = Number(process.env.PORT || 5000);
-app.listen(port|| 3000);
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
 console.log('Listening on port 3000...');
